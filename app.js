fcf2cd97fa6672fedb1247f595f04d7c28b1a147
ObodoakorDavid <obodoakordavid@outlook.com>
2025-01-19 19:51:30 +0100

    added url extended

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(
   fileUpload({
